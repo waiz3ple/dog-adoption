d6b525c5e0a2e5de5cc5bdf87b3ff84df8d67e70
Wasiu Ramoni <wasiu.a.ramoni@gmail.com>
2025-03-21 16:00:51 -0400

    Config: config vite enviroment to support tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Login } from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      nothing
+      <Login />
     </>
   )
 }
-
 export default App
